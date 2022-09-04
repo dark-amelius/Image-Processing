da53f875cc54d18621fe8f7d12d05367bf35ed46
Hiram Artnak Martins <hiram.artnak@gmail.com>
2022-09-04 18:46:05 -0300

    hsv stretch

--- src/ex2.py
+++ src/ex2.py
@@ -63,9 +63,11 @@ def contrast_stretching(img, is_colored=False):
     return i
 
 
-def get_histogram(img):
-
-    img = img.convert('L')
+def get_histogram(img, is_colored=False):
+    if is_colored:
+        img = img.convert('HSV')
+    else:
+        img = img.convert('L')
     width, height = img.size
     histogram = [0]*256
     num = [0]*256
@@ -75,14 +77,17 @@ def get_histogram(img):
 
     for x in range(width):
         for y in range(height):
-            p = img.getpixel((x, y))
+            if is_colored:
+                p = img.getpixel((x, y))[2]
+            else:
+                p = img.getpixel((x, y))
             histogram[p] += 1
 
     return histogram
 
 
-def cumulative_histogram(img):
-    histogram = get_histogram(img)
+def cumulative_histogram(img, is_colored=False):
+    histogram = get_histogram(img, is_colored)
     cumulative_hist = [0.0]*256
     width, height = img.size
     num_pixels = width*height
@@ -95,9 +100,12 @@ def cumulative_histogram(img):
     return cumulative_hist
 
 
-def histogram_equalization(img):
-    img = img.convert("L")
-    histogram = get_histogram(img)
+def histogram_equalization(img, is_colored=False):
+    if is_colored:
+        img = img.convert("HSV")
+    else:
+        img = img.convert("L")
+    histogram = get_histogram(img, is_colored)
     i = img
     cumulative_hist = [0]*256
     width, height = img.size
@@ -111,7 +119,10 @@ def histogram_equalization(img):
     for w in tqdm(range(width)):
         for h in range(height):
             c = img.getpixel((w, h))
-            i.putpixel((w, h), (int(cumulative_hist[c])))
+            if is_colored:
+                v = (c[0], c[1], (int(cumulative_hist[c[2]])))
+            else:
+                i.putpixel((w, h), (int(cumulative_hist[c])))
 
     return i
 
@@ -124,6 +135,9 @@ img = contrast_stretching(RICE)
 img.save(results.joinpath('rice-stretching.jpg'))
 print()
 
+img = histogram_equalization(KIKI, is_colored=True).convert('RGB')
+img.save(results.joinpath('Kiki-histo.jpg'))
+print()
 
 img = contrast_stretching(KIKI, is_colored=True).convert('RGB')
 img.save(results.joinpath('Kiki-stretch.jpg'))
